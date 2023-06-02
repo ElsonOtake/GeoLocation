a5b7adc472bf480cc1c21099faae7a156be179b2
Elson Otake <elsonotake@yahoo.com.br>
2023-06-02 11:30:53 -0300

    Add geolocation api code

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,7 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
 // Connects to data-controller="geolocation"
 export default class extends Controller {
-  connect() {
+  search() {
+    navigator.geolocation.getCurrentPosition(this.success, this.error, options);
+  }
+  
+  success(pos) {
+    const crd = pos.coords;
+  
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 }
