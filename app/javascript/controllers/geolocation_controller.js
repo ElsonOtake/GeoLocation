3f8336e997a132451f2f82a63587815ad76be366
Elson Otake <elsonotake@yahoo.com.br>
2023-07-25 13:50:00 -0300

    Add google maps call

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,13 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
+  // const options = {
+  //   enableHighAccuracy: true,
+  //   timeout: 5000,
+  //   maximumAge: 0,
+  // };
 
 // Connects to data-controller="geolocation"
 export default class extends Controller {
+  static values = { page: String }
+
+  connect() {
+    switch(this.pageValue) {
+      case "simple_map":
+        this.simple_map();
+        break;
+      default:
+        console.log("page not found!");
+    }
+  }
   search() {
     navigator.geolocation.getCurrentPosition(this.success, this.error, options);
   }
@@ -24,5 +35,12 @@ export default class extends Controller {
   
   error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  simple_map() {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
   }
 }
