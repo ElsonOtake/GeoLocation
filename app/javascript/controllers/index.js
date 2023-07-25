35f66502406c532049a5f702c0d12e783792cbf9
Elson Otake <elsonotake@yahoo.com.br>
2023-07-25 16:58:45 -0300

    Remove unused controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,3 @@ import { application } from "./application"
 
 import GeolocationController from "./geolocation_controller"
 application.register("geolocation", GeolocationController)
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
