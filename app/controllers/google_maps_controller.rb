a6df47eb1cc11284d365e7fabb6965bfe105d409
Elson Otake <elsonotake@yahoo.com.br>
2023-07-25 17:04:39 -0300

    Remove unnecessary empty actions

--- app/controllers/google_maps_controller.rb
+++ app/controllers/google_maps_controller.rb
@@ -1,17 +1,3 @@
 class GoogleMapsController < ApplicationController
-  # def map;end
 
-  # def simple_map;end
-
-  # def pixel_tile;end
-
-  # def geolocation;end
-
-  # def localizing;end
-
-  # def right_to_left;end
-
-  # def custom;end
-
-  # def literal;end
 end
